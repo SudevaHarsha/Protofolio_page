d74344b032dc01413eec8632cdeb8034c3e789e5
SudevaHarsha <sudevaharsha1@gmail.com>
2023-04-01 09:30:06 +0530

    sudeva

--- protofolio/src/pages/HomePage.jsx
+++ protofolio/src/pages/HomePage.jsx
@@ -3,8 +3,13 @@ import Navbar from '../Navbar';
 
 const HomePage = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div className='hero_section'>
+        <img src="https://picsum.photos/200/300"></img>
+      </div>
+    </>
   )
-}
+};
 
 export default HomePage;
